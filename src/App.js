9fb4ed261ad79898b0c435fb4facfbad0b9b3a00
adrianezd <adrianezquerrods@gmail.com>
2023-01-24 22:26:37 +0100

    simpleStates [cont, name, age, image]

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [nombre, setName] = useState("");
+  const [edad, setEdad] = useState(0);
+  const [imagen, setImagen] = useState(
+    "https://developero.io/_next/image?url=%2Fstatic%2Fimages%2Fprincipios-solid-react-js%2FPrincipios-SOLID-en-React-JS.png&w=1200&q=75"
+  );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEdad(agefield.value);
+  };
+
+  const handleSubmitImage = (e) => {
+    e.preventDefault();
+    imagen === "https://developero.io/_next/image?url=%2Fstatic%2Fimages%2Fprincipios-solid-react-js%2FPrincipios-SOLID-en-React-JS.png&w=1200&q=75"
+      ? setImagen(
+          "https://cloud.panoimagen.com/static/img/BBTWINS-tagline.ico"
+        )
+      : setImagen(
+          "https://developero.io/_next/image?url=%2Fstatic%2Fimages%2Fprincipios-solid-react-js%2FPrincipios-SOLID-en-React-JS.png&w=1200&q=75"
+        );
+  };
+  
+  var agefield = document.getElementById("agefield");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>REACT APP</h1>
+          <p>Count: {count}</p>
+          <button onClick={() => setCount(count + 1)}>+</button>
+          <button onClick={() => setCount(count - 1)}>-</button>
+        </div>
+        <div>
+          <p>Mi nombre es: {nombre}</p>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            id="inputfield"
+          ></input>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <p>Mi edad es: {edad}</p>
+            <input id="agefield"></input>
+            <br></br>
+            <button className="button" onClick={() => setEdad(agefield)}>
+              NUEVA EDAD
+            </button>
+          </div>
+        </form>
+        <form onSubmit={handleSubmitImage}>
+          <img width="300" height="200" src={imagen}></img>
+          <button onClick={() => setImagen(imagen)} className="button">
+            CAMBIAR IMAGEN
+          </button>
+        </form>
       </header>
     </div>
   );
